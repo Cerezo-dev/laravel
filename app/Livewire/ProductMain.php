e9a5cdbe79213c890245859c5e357405869768d1
Cerezo-dev <unacuentamasuwux@gmail.com>
2026-06-12 11:32:17 -0500

    feat: se agrega CRUD para productos con retroalimenatcion visual

--- app/Livewire/ProductMain.php
+++ app/Livewire/ProductMain.php
@@ -3,34 +3,107 @@
 namespace App\Livewire;
 
 use App\Models\Product;
-use Livewire\Component;
+use Livewire\Attributes\On;
 use Livewire\Attributes\Validate;
+use Livewire\Component;
+use Flux\Flux;
+use Livewire\WithPagination;
 
-class ProductMain extends Component {
+class ProductMain extends Component{
+    use WithPagination;
 
-public $search, $descripcion;
+    public $search,$descripcion,$id;
+    public $productSelectedId = null;
 
-#[Validate('required|min:4')]
-public $nombre;
-#[Validate('required')]
-public $disponible;
-#[Validate('required|numeric|min:1')]
-public $cantidad, $precio;
+    #[Validate('required')]
+    public $nombre,$cantidad,$precio,$disponible;
 
     public function render(){
-        $productos=Product::where('nombre', 'LIKE', '%'.$this->search. '%') -> paginate();
+        $productos=Product::where('nombre','LIKE','%'.$this->search.'%')
+        ->latest()->paginate();
         return view('livewire.product-main',compact('productos'));
     }
 
     public function save(){
         $this->validate();
-        Product::create([
-        'nombre'=>$this->nombre,
-        'descripcion'=>$this->descripcion,
-        'cantidad'=>$this->cantidad,
-        'precio'=>$this->precio,
-        'disponible'=>$this->disponible
+        if(!$this->id){
+            Product::create([
+                'nombre'=>$this->nombre,
+                'descripcion'=>$this->descripcion,
+                'cantidad'=>$this->cantidad,
+                'precio'=>$this->precio,
+                'disponible'=>$this->disponible
+            ]);
+            Flux::toast(
+                heading: 'Producto registrado.',
+                text: 'El registro se realizo correctamente.',
+                variant: 'success'
+            );
+        }else{
+            $producto=Product::find($this->id);
+            $producto->update([
+                'nombre'=>$this->nombre,
+                'descripcion'=>$this->descripcion,
+                'cantidad'=>$this->cantidad,
+                'precio'=>$this->precio,
+                'disponible'=>$this->disponible
+            ]);
+            Flux::toast(
+                heading: 'Producto actualizado.',
+                text: 'El registro se actualizo correctamente.',
+                variant: 'success'
+            );
+        }
 
-        ]);
+        $this->modal('showform')->close();
+    }
+
+    public function edit(Product $item){
+        $this->id=$item->id;
+        $this->nombre=$item->nombre;
+        $this->descripcion=$item->descripcion;
+        $this->cantidad=$item->cantidad;
+        $this->precio=$item->precio;
+        $this->disponible=$item->disponible;
+        $this->modal('showform')->show();
+    }
+
+    public function create(){
+        $this->reset(['id','nombre','descripcion','cantidad','precio','disponible']);
+        $this->modal('showform')->show();
+    }
+
+    public function openUpload(Product $item){
+        $this->productSelectedId = $item->id;
+        $this->modal('showUpload')->show();
+    }
+
+    #[On('image-uploaded')]
+    public function onImageUploaded(){
+        $this->modal('showUpload')->close();
+    }
+
+    public function confirm(Product $item){
+        $this->id=$item->id;
+        $this->modal('delete-profile')->show();
+    }
+
+    public function delete(){
+        $producto=Product::find($this->id);
+        /*
+        $producto->update([
+            'disponible'=>false //Soft delete
+        ]);*/
+        $producto->delete(); //hard delete
+        Flux::toast(
+            heading: 'Producto eliminado.',
+            text: 'El registro se borró correctamente.',
+            variant: 'success'
+        );
+        $this->modal('delete-profile')->close();
+    }
+
+    public function updatingSearch(): void{
+        $this->resetPage();
     }
 }
--- resources/views/livewire/product-main.blade.php
+++ resources/views/livewire/product-main.blade.php
@@ -2,11 +2,7 @@
     <h1 class="text-3xl mb-10 border-b-3 pb-1 border-violet-500">Gestión de productos</h1>
     <div class="flex gap-2 mb-4">
         <flux:input wire:model.live="search" placeholder="Buscar producto" icon="magnifying-glass"/>
-
-        <flux:modal.trigger name="showform">
-            <flux:button variant="primary" color="violet" icon="plus">Nuevo</flux:button>
-        </flux:modal.trigger>
-
+        <flux:button wire:click="create()" variant="primary" color="violet" icon="plus" class="cursor-pointer">Nuevo</flux:button>
     </div>
     <flux:table>
         <flux:table.columns>
@@ -15,6 +11,7 @@
             <flux:table.column>CANTIDAD</flux:table.column>
             <flux:table.column>PRECIO</flux:table.column>
             <flux:table.column>DISPONIBLE</flux:table.column>
+            <flux:table.column>OPCIONES</flux:table.column>
         </flux:table.columns>
 
         <flux:table.rows>
@@ -31,6 +28,11 @@
                             {{ $item->disponible?"SI":"NO" }}
                         </flux:badge>
                     </flux:table.cell>
+                    <flux:table.cell>
+                        <flux:button wire:click="openUpload({{ $item}})" icon="photo" class="cursor-pointer"></flux:button>
+                        <flux:button wire:click="edit({{ $item}})" variant="primary" color="amber" icon="pencil" class="cursor-pointer"></flux:button>
+                        <flux:button wire:click="confirm({{ $item}})" variant="primary" color="red" icon="trash" class="cursor-pointer"></flux:button>
+                    </flux:table.cell>
                 </flux:table.row>
             @endforeach
         </flux:table.rows>
@@ -38,6 +40,7 @@
     <div>
         {{$productos->links()}}
     </div>
+
     <flux:modal name="showform" flyout>
         <div class="space-y-6">
             <div>
@@ -48,11 +51,41 @@
             <flux:input wire:model="cantidad" label="Cantidad" placeholder="12"/>
             <flux:input wire:model="precio" label="Precio" placeholder="45.50"/>
             <flux:checkbox wire:model="disponible" label="Disponible"/>
-            <div class="flex">
+            <div class="flex gap-1">
                 <flux:spacer />
+                <flux:button x-on:click="$flux.modal('showform').close()">Cancelar</flux:button>
                 <flux:button wire:click="save()" variant="primary" color="violet" icon="arrow-turn-down-right">Guardar</flux:button>
             </div>
         </div>
     </flux:modal>
-</div>
 
+    <flux:modal name="delete-profile" class="min-w-[22rem]">
+        <div class="space-y-6">
+            <div>
+                <flux:heading size="lg">Delete project?</flux:heading>
+                <flux:text class="mt-2">
+                    You're about to delete this project.<br>
+                    This action cannot be reversed.
+                </flux:text>
+            </div>
+            <div class="flex gap-2">
+                <flux:spacer />
+                <flux:modal.close>
+                    <flux:button variant="ghost">Cancel</flux:button>
+                </flux:modal.close>
+                <flux:button wire:click="delete()" variant="danger">Borrar registro</flux:button>
+            </div>
+        </div>
+    </flux:modal>
+
+    <flux:modal name="showUpload" class="md:w-96">
+        <div class="space-y-6">
+            <div>
+                <flux:heading size="lg">Imágenes del producto</flux:heading>
+            </div>
+            @if($productSelectedId)
+                @livewire('image-uploader', ['model' => \App\Models\Product::find($productSelectedId)], key($productSelectedId))
+            @endif
+        </div>
+    </flux:modal>
+</div>
