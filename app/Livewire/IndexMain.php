8c2d80a4c574830464bf041eafd62d315e9dff85
Cerezo-dev <unacuentamasuwux@gmail.com>
2026-06-26 10:35:44 -0500

    sjkhd

---
+++ app/Livewire/IndexMain.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class IndexMain extends Component
+{
+    //#[Layout('layouts.index')]
+    public function render()
+    {
+        return view('livewire.index-main');
+    }
+}
